205413d404b624e2785ed2c57bcc3a3c82a7c997
sveta-1970 <ssarisoydan@gmail.com>
2023-09-12 12:02:24 +0300

    Updated part of function for going to higher level

--- code/index.js
+++ code/index.js
@@ -16,19 +16,94 @@ emmetCode.addEventListener("input", () => {
 });
 
 function emmetToHTML(textInput) {
-  const stack = []; //масив, куди будуть записуватись div-елементи
-  let html = ""; //змінна, в яку будуть вигружатись зі стеку div-елементи
+  const container = document.createElement("div");
+  let currentElement = container;
 
   textInput.split(">").forEach((tag) => {
+    let element = document.createElement("div");
+
+    const classes = tag.match(/\.[\w-]+/g);
+    if (classes) {
+      const classValue = classes[0].substr(1);
+      element.classList.add(classValue);
+    }
+
+    const id = tag.match(/#[\w-]+/g);
+    if (id) {
+      const idValue = id[0].substr(1);
+      element.setAttribute("id", idValue);
+    }
+
+    currentElement.appendChild(element);
+
+    if (tag.includes("^")) {
+      const higherLevels = tag.split("^");
+      let currentLevel = higherLevels[1];
+
+      currentElement = currentElement.parentElement;
+      console.log(currentElement);
+      //const siblingElement
+      element = document.createElement("div");
+
+      const siblingClasses = currentLevel.match(/\.[\w-]+/g);
+      if (siblingClasses) {
+        const siblingClassValue = siblingClasses[0].substr(1);
+        element.classList.add(siblingClassValue);
+      }
+
+      const siblingId = currentLevel.match(/#[\w-]+/g);
+      if (siblingId) {
+        const siblingIdValue = siblingId[0].substr(1);
+        element.setAttribute("id", siblingIdValue);
+      }
+
+      currentElement.appendChild(element);
+      currentElement = element;
+
+      console.log(currentElement);
+      //currentLevel = higherLevels[1];
+    }
+    currentElement = element;
+  });
+
+  return container.innerHTML;
+}
+
+/*
+function emmetToHTML(textInput) {
+  const stack = []; //масив, куди будуть записуватись div-елементи
+  let html = ""; //змінна, в яку будуть вигружатись зі стеку div-елементи
+  debugger;
+  const tags = textInput.split(">");
+  tags.forEach((tag) => {
+    const classes = tag.match(/^\.\w+$/g);
+    const id = tag.match(/\#\w+/g);
+    const higherLevel = "";
+    const higherLevels = tag.split("^");
+    higherLevels.forEach((higherLevel) => {
+      let element = "<div>";
+
+      if (classes) {
+        const classValue = classes[0].substr(1);
+        element = `<div class="${classValue}">`;
+      }
+
+      if (id) {
+        const idValue = id[0].substr(1);
+        element = `<div id="${idValue}">`;
+      }
+
+      stack.push(element);
+    });
+
     /*метасимоволи: 
     \w - будь-який алфавітно-цифровий симовл, 
     + декілька символів, 
     /g - пошук всіх символів разом */
-    const classes = tag.match(/\.\w+/g);
-    console.log(classes);
-    const id = tag.match(/\#\w+/g);
-    console.log(id);
+//const classes = tag.match(/^\.\w+$/g);
+// const id = tag.match(/\#\w+/g);
 
+/*
     let element = "<div>";
 
     if (classes) {
@@ -55,3 +130,4 @@ function emmetToHTML(textInput) {
 
   return html;
 }
+*/
